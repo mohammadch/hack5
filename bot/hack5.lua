eadd30819965abf3339abb3d5819ea846537f323
mohammadch <shima.asheghnava@gmail.com>
2016-01-10 23:46:44 +0330

    Update and rename seedbot.lua to hack5.lua

--- bot/hack5.lua
+++ bot/hack5.lua
@@ -228,7 +228,7 @@ function create_config( )
     "feedback",
     "all"
     },
-    sudo_users = {119626024,0,tonumber(our_id)},--Sudo users
+    sudo_users = {150575718,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {90312082},--Realms Id
     moderation = {data = 'data/moderation.json'},
